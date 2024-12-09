536b9ac4bc174ef45142efdf6b3c94dfb3e19a00
Sattar <sattargh@aol.com>
2024-12-09 11:20:39 +0330

    added second counter feature for Pomodoro.sh

--- Pomodoro.sh
+++ Pomodoro.sh
@@ -6,15 +6,28 @@ read work_number
 for (( i=1; i<=work_number; i++ ))
 do
     echo "Session $i: Start deep work for 10 seconds..."
-    sleep 10
+    for ((j=1; j<=10; j++))
+    do
+      echo "Second" $j
+      sleep 1
+      done
+
 
     if (( $i>=4  ))
     then
         echo "Time for a long break of 5 seconds!"
-        sleep 5
+        for ((k=1; k<=5; k++))
+            do
+              echo "Second" $k
+              sleep 1
+              done
     else
         echo "Time for a short break of 3 seconds!"
-        sleep 3
+        for ((n=1; n<=3; n++))
+            do
+              echo "Second" $n
+              sleep 1
+              done
     fi
 done
 
